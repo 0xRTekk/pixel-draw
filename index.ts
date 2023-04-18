c565c1443e3aa283ce1bcaaa3450aa917e2c6b39
RemOclock <remi.sulpice@oclock.io>
2023-04-18 07:44:01 +0000

    Petit serveur express

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log('coucou');
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Starship");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
